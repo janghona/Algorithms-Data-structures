4e367389be6ddfcd5efe095a156c948c6afc3177
janghona <jang1na2@gmail.com>
2021-08-11 17:33:38 +0900

    Fibonacci Search(피보나치 검색)
    * 피보나치 수열 F[2] = F[0] + F[1] 이용한 간격 검색 방법
    - 1/3씩 검색을 함 offset = -1 값을 가지고 있음
    - 시간복잡도는 logn

---
+++ algorithm/algorithm.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacciSearch(int arr[], int target, int len)
+{
+    int fbM2 = 0;
+    int fbM1 = 1;
+    int fbM = fbM2 + fbM1;
+    int offset = -1;
+    while (fbM < len)
+    {
+        fbM2 = fbM1;
+        fbM1 = fbM;
+        fbM = fbM2 + fbM1;
+    }
+    while (fbM > 1)
+    {
+        int i = min(offset + fbM2, len - 1);
+        if (arr[i] < target)
+        {
+            fbM = fbM1;
+            fbM1 = fbM2;
+            fbM2 = fbM - fbM1;
+            offset = i;
+        }
+        else if (arr[i] > target)
+        {
+            fbM = fbM2;
+            fbM1 = fbM1 - fbM2;
+            fbM2 = fbM - fbM1;
+        }
+        else return i;
+    }
+    if (fbM1 && arr[offset + 1] == target)
+        return offset + 1;
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int index = 0;
+    int inputTarget = 0;
+
+    cout << "input target number(1~9) : ";
+    cin >> inputTarget;
+    int index = fibonacciSearch(arr, inputTarget, sizeof(arr)/4);
+    if (index == -1) cout << "Fail !!";
+    else {
+        cout << "Element found at index " << index;
+    }
+    return 0;
+}
