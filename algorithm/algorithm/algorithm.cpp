3f54905470ff22e9d02a6fb340b5ef7bce70a6f7
janghona <jang1na2@gmail.com>
2021-08-15 21:36:47 +0900

    Kruskal 알고리즘
    * 최소 스패닝 트리(Minimum Spanning Tree) : 모든 정점을 포함하는 서브트리 형태
    * Kruskal Algorithm : 최소 스패닝 트리 중 간선 가중치 합이 최소인것
    * union-find 알고리즘 사용(간선이 이미 있는지 없는지 여부) / 정렬된 상태로 가중치 값을 vector에 푸시함

--- algorithm/algorithm/algorithm.cpp
+++ algorithm/algorithm/algorithm.cpp
@@ -1,45 +1,70 @@
 ﻿#include <iostream>
 #include<stdio.h>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int InterpolationSearch(int arr[], int len, int target)
-{
-
-    int low = 0, high = (len - 1);
-    int pos = 0;
-
-    while (arr[low] != arr[high] && target >= arr[low] && target <= arr[high])
-    {
-        pos = low + (target - arr[low]) * (high - low) / (arr[high] - arr[low]);
-
-        if (arr[pos] == target)
-            return pos;
-        else if (arr[pos] > target)
-            high = pos - 1;
-        else
-            low = pos + 1;
-    }
-
-    if (target == arr[low])
-        return low;
-    else
-        return -1;
+int getParent(int set[], int x) {
+	if (set[x] == x) return x;
+	return set[x] = getParent(set, set[x]);
 }
 
-int main()
-{
-    int arr[] = { 1,2,3,7,9,12,21,23,27 };
-    int index = 0;
-    int inputTarget = 0;
-
-    printf("input number : ");
-    scanf_s("%d", &inputTarget);
-
-    index = InterpolationSearch(arr, sizeof(arr), inputTarget);
-    if (index == -1) printf("Fail");
-    else {
-        printf("arr[] target index : %d", index);
-    }
+void merge(int set[], int a, int b) {
+	a = getParent(set, a);
+	b = getParent(set, b);
+	if (a < b) set[b] = a;
+	else set[a] = b;
 }
 
+bool isSameParent(int set[], int a, int b) {
+	a = getParent(set, a);
+	b = getParent(set, b);
+	if (a == b) return true;
+	else return false;
+}
 
+struct Edge {
+	int node[2];
+	int cost;
+
+	Edge(int v1, int v2, int cost) {
+		this->node[0] = v1;
+		this->node[1] = v2;
+		this->cost = cost;
+	}
+	bool operator < (const Edge& e1) const {
+		return cost < e1.cost;
+	}
+};
+
+int main(){
+	const int n = 7;
+	int m = 11;
+	vector<Edge> e;
+
+	e.push_back(Edge(1, 7, 12));
+	e.push_back(Edge(1, 4, 28));
+	e.push_back(Edge(1, 2, 67));
+	e.push_back(Edge(1, 5, 17));
+	e.push_back(Edge(2, 4, 24));
+	e.push_back(Edge(2, 5, 62));
+	e.push_back(Edge(3, 5, 20));
+	e.push_back(Edge(3, 6, 37));
+	e.push_back(Edge(4, 7, 13));
+	e.push_back(Edge(5, 6, 45));
+	e.push_back(Edge(5, 7, 73));
+
+	sort(e.begin(), e.end());
+
+	int set[n];
+	for (int i = 0; i < n; i++) set[i] = i;
+	int sum = 0;
+	for (int i = 0; i < e.size(); i++) {
+		if (!isSameParent(set,e[i].node[0]-1, e[i].node[1] - 1)){
+			sum += e[i].cost;
+			merge(set, e[i].node[0] - 1, e[i].node[1] - 1);
+		}
+	}
+
+	printf("MST : %d\n", sum);
+}
