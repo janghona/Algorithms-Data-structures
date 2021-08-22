ad85649fac49db118d11db826049d0756229f288
janghona <jang1na2@gmail.com>
2021-08-22 18:06:44 +0900

    Merge Sort(합병 정렬 알고리즘)
    * 합병정렬 또는 병합정렬이라고 하며 분할 정복 알고리즘종류임
    * 원소1개 단위까지 분할 후 인접한 원소 끼리 병합하며 정렬을 함

--- algorithm/algorithm/algorithm.cpp
+++ algorithm/algorithm/algorithm.cpp
@@ -2,49 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-void swap(int* v1, int* v2) {
-	int temp;
-	temp = *v1;
-	*v1 = *v2;
-	*v2 = temp;
-}
-void downheap(int cur, int k,int* data){
-	int left, right, p;
-	while (cur < k) {
-		left = cur * 2 + 1;
-		right = cur * 2 + 2;
+void mergeSort(int A[], int low, int high, int B[]){
+	if (low >= high) return;
+	int mid = (low + high) / 2;
 
-		if (left >= k && right >= k) break;
-
-		p = cur;
-		if (left < k && data[p] < data[left]) p = left;
-		if (right < k && data[p] < data[right]) p = right;
-		if (p == cur) break;
-
-		swap(&data[cur], &data[p]);
-		cur = p;
+	mergeSort(A, low, mid, B);
+	mergeSort(A, mid + 1, high, B);
+	int i = low, j = mid + 1, k = low;
+	for (; k <= high; ++k) {
+		if (j > high) B[k] = A[i++];
+		else if (i > mid) B[k] = A[j++];
+		else if (A[i] <= A[j]) B[k] = A[i++];
+		else B[k] = A[j++];
 	}
-}
-
-void heapify(int* data,int n){
-	int i, p;
-	for (i = (n - 1) / 2; i >= 0; i--) {
-		downheap(i, n,data);
-	}
-	//for(i=0;i<size;++i)printf("%d ",data[i]);
-	//printf("\n");
-}
-
-void heapSort(int* data, int size){
-	int k;
-	heapify(data, size);
-	for (k = size - 1; k > 0; k--) {
-		swap(&data[0], &data[k]);
-		downheap(0, k,data);
-	}
+	for (i = low; i <= high; ++i) A[i] = B[i];
 }
 int main(){
 	int list[] = { 5,8,6,4,1,3,2,7,10,9 };
+	int B[sizeof(list)/4];
 
 	printf("정렬 전 : ");
 	for (int i = 0; i < sizeof(list) / 4; i++) {
@@ -52,7 +27,7 @@ int main(){
 	}
 	printf("\n");
 	printf("정렬 후 : ");
-	heapSort(list,sizeof(list)/4);
+	mergeSort(list,0,(sizeof(list)/4)-1,B);
 	for (int i = 0; i < sizeof(list) / 4; i++) {
 		printf("%d ",list[i]);
 	}
