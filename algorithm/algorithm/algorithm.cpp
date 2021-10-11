20502cab6fc336ec880ed8917571d50eff8d7ad4
janghona <jang1na2@gmail.com>
2021-10-12 05:00:32 +0900

    자료구조 <list><set>문제(2020 제2회 게임프로그래밍전문가 실기 1번)
    * 자료구조 list와 set을 활용한 add(), remove(), reverse() 문제

--- algorithm/algorithm/algorithm.cpp
+++ algorithm/algorithm/algorithm.cpp
@@ -1,93 +1,137 @@
-﻿// 프림알고리즘(Prim's algorithm)
+﻿#include <windows.h>
+#include <tchar.h>
+#include <list>
+#include <set>
 
-#include <iostream>
-using namespace std;
-
-#define marked 		-1 		// 방문여부 확인, 싸이클이 형성 방지용
-#define NODE_NUM	7 		// 노드(정점)의 수
-#define INF 		9999	// 무한대
-
-int primData[NODE_NUM][NODE_NUM] =
+//아이템 클래스
+class Item
 {
-	{ 0, 14, 22, 21, INF, INF, INF},
-	{ 14, 0, 43, INF, INF, INF, 47},
-	{ 22, 43, 0, INF, 31, INF, INF},
-	{ 21, INF, INF, 0, 50, 55, INF},
-	{ INF, INF, 31, 50, 0, INF, 15},
-	{ INF, INF, INF, 55, INF, 0, 24},
-	{ INF, 47, INF, INF, 15, 24, 0 }
-}; // 인접 행렬
+	int ItemNO; //아이템 번호
+	int Count; //아이템 개수
 
-void PRIM(int n, int distance[NODE_NUM][NODE_NUM]);// 함수의 정의 변경
+public:
+	Item(int itemNO, int count) : ItemNO(itemNO), Count(count) {}
 
+	int GetItemNO() const { return ItemNO; }
+	int GetCount() const { return Count; }
+	void AddCount(int add) { Count += add; }
+
+	void Print()
+	{
+		printf("Item : ItemNO(%d), Count(%d)\n", ItemNO, Count);
+	}
+};
+
+//인벤토리 클래스
+class Inventory
+{
+	std::list<Item*> ItemList; //아이템 리스트
+
+public:
+	//모든 아이템을 출력하는 함수
+	void Print()
+	{
+		if (ItemList.empty())
+		{
+			printf("Empty");
+			return;
+		}
+
+		for (auto it = ItemList.begin(); it != ItemList.end(); ++it)
+		{
+			(*it)->Print();
+		}
+	}
+
+	//아이템을 찾는 함수
+	Item* GetItem(int itemNO)
+	{
+		for (auto it = ItemList.begin(); it != ItemList.end(); ++it)
+		{
+			if ((*it)->GetItemNO() == itemNO)
+			{
+				return (*it);
+			}
+		}
+		return nullptr; //일치하는 아이템이 없을 경우 nullptr 반환
+	}
+
+	//아이템을 추가하는 함수
+	void AddItem(int itemNO, int count)
+	{
+		//---------------------------------------------------------------------------------------------------------------
+		//★ 문제 1
+		//매개변수로 전달된 itemNO 와 같은 아이템이 있을 경우 해당 아이템의 Count 를 매개변수로 전달된 count 만큼 증가시키고,
+		//없을 경우 새로운 아이템을 추가하시오.
+		//---------------------------------------------------------------------------------------------------------------
+
+		if (GetItem(itemNO) != nullptr) {
+			GetItem(itemNO)->AddCount(count);
+		}
+		else if (GetItem(itemNO) == nullptr) {
+			Item* instance = new Item(itemNO, count);
+			ItemList.push_back(instance);
+		}
+	}
+
+	//아이템을 제거하는 함수
+	void RemoveItems(std::set<int> removeList)
+	{
+		//---------------------------------------------------------------------------------------------------------------
+		//★ 문제 2
+		//매개변수로 전달된 removeList 와 일치하는 모든 아이템을 제거하시오. 동적 할당이 되어있으므로 delete 처리도 필요함!
+		//단, removeList 에는 제거하려는 itemID 들이 들어있음.
+		//---------------------------------------------------------------------------------------------------------------
+		for (auto iter = removeList.begin(); iter != removeList.end(); iter++) {
+			Item* tempItem = GetItem(*iter);
+			ItemList.remove(tempItem);
+			delete tempItem;
+		}
+	}
+
+	//아이템 리스트를 역순으로 변경하는 함수
+	void ReverseItems()
+	{
+		//---------------------------------------------------------------------------------------------------------------
+		//★ 문제 3
+		//아이템 리스트의 순서를 역순으로 변경하시오. 
+		//---------------------------------------------------------------------------------------------------------------
+
+		ItemList.reverse();
+	}
+};
 
 int main()
 {
-	// 아래의 수험번호, 성명 수정하여 입력해야 함
-	//system("mode con cols=80 lines=30 | title KOCCA [Problem 2]");
-	cout << "■ [문제2] 모범답안 \n■ 수험번호: 213123000002 \n■ 성    명: 나장호\n";
-	cout << "\n■  Prim Algrithm ■ \n";
+	system("cls");
+	SetConsoleTitle(_T("문제"));
 
-	cout << "==Prim's algorithm MST(Minimum Spanning Tree)==" << endl;
-	PRIM(NODE_NUM, primData);
+	//★ [필수] 수험생의 수험번호와 이름을 여기에 기입하여 출력될 수 있도록 함
+	//   [주의] 수험번호와 이름이 출력되지 않을 경우 오답처리!
+	printf(">> 수험번호 : \n");
+	printf(">> 이    름 : 나장호 \n\n");
 
-	//scanf_s("%d");
+	Inventory inven;
+	inven.Print();
+
+	inven.AddItem(4, 300);
+	inven.AddItem(1, 50);
+	inven.AddItem(2, 100);
+	inven.AddItem(1, 50);
+	inven.AddItem(3, 300);
+	inven.AddItem(2, 100);
+	inven.AddItem(5, 500);
+	inven.AddItem(4, 100);
+	printf("\n\nAfter add...\n");
+	inven.Print();
+
+	inven.RemoveItems({ 2, 3 });
+	printf("\nAfter remove...\n");
+	inven.Print();
+
+	inven.ReverseItems();
+	printf("\nAfter reverse...\n");
+	inven.Print();
+
+	getchar();
 }
-
-
-void PRIM(int n, int distance[NODE_NUM][NODE_NUM])
-{
-	int nNodeNum, nCheckPrimNode, nCheckCycleNdoe, nCntPrimNode, primNode[NODE_NUM];
-	int nMinDist[NODE_NUM], nNewMin;
-	int nSumWeight = 0;
-
-	for (nNodeNum = 1; nNodeNum < n; nNodeNum++) {
-		nMinDist[nNodeNum] = distance[0][nNodeNum]; 	// 시작점
-		primNode[nNodeNum] = 0; 						//시작 초기값 0번째 정점
-	}
-
-	for (nNodeNum = 1; nNodeNum < n; nNodeNum++) {
-		nCntPrimNode = 1; 								// 가장 가까운 노드(정점) 값 1
-		nNewMin = nMinDist[1];
-
-		for (nCheckPrimNode = 2; nCheckPrimNode < n; nCheckPrimNode++)
-			if (nMinDist[nCheckPrimNode] < nNewMin) {
-				nNewMin = nMinDist[nCheckPrimNode];
-				//* ★
-				// 가중치가 작은값이 있을 때 그 값으로 갱신하는 코드 작성
-				// ★코드 작성  // 문제 2-1) 10점
-				//*/
-				nCntPrimNode = nCheckPrimNode; 			// 해당 정점노드를 프림연결노드(정점)로 갱신
-
-			}
-
-		cout << nCntPrimNode << " 번 노드의 가중치(edge):" << " ";
-		cout << nNewMin; 								// 가중치 = 최소비용 = 최단거리 = 최소시간
-		cout << "\n";
-
-		nSumWeight = nSumWeight + nNewMin; 				// 가중치 합계
-
-		primNode[nCntPrimNode] = marked;				//방문하면 -1로 처리, 싸이클이 형성되지 않게 하기위해서
-		nMinDist[nCntPrimNode] = INF; 					//초기값으로 가중치 무한대값 9999 대입
-
-		for (nCheckCycleNdoe = 1; nCheckCycleNdoe < n; nCheckCycleNdoe++) {
-
-			//* ★
-			// 인접한 노드 중에서 사이클이 형성되지 않도록 빈곳을 완성하시오.
-			if ((primNode[nCheckCycleNdoe] != marked) && (nMinDist[nCheckCycleNdoe] > distance[nCntPrimNode][nCheckCycleNdoe])
-				// &&
-				// ( ★ 여기에 조건 코드 작성 // 문제 2-2) 5점  )
-				)
-			{
-				nMinDist[nCheckCycleNdoe] = distance[nCntPrimNode][nCheckCycleNdoe];
-				// 최소값으로 가중치 갱신하는 코드 작성하시오.
-				//
-				// ★여기에 코드 작성 // 문제 2-3) 5점
-				//
-				primNode[nCheckCycleNdoe] = nCntPrimNode;							// 최소값을 가진 가중치의 정점(노드)
-			}
-		}
-	}
-	cout << "\n 가중치 합= " << nSumWeight;
-}
-
