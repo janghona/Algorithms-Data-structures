8cfecbee352639550d6810675913018d1315b084
janghona <jang1na2@gmail.com>
2021-11-04 07:44:47 +0900

    Priority Queue 문제(2021 제1회 게임프로그래밍전문가 실기 2번)
    * 기본적으로 priority queue는 큰 수부터 pop을 함
    * 작은 값부터 pop을 원할 시 : priority_queue<자료형, vector<자료형>, 비교값> 형태로 사용
    * 비교값은 bool형 operator< / operator() 을 사용하여 return 값을 비교 함

--- algorithm/algorithm/algorithm.cpp
+++ algorithm/algorithm/algorithm.cpp
@@ -1,137 +1,131 @@
-﻿#include <windows.h>
-#include <tchar.h>
-#include <list>
-#include <set>
+﻿#include <iostream>
+#include <queue>
 
-//아이템 클래스
-class Item
-{
-	int ItemNO; //아이템 번호
-	int Count; //아이템 개수
+using namespace std;
 
+class Job {
 public:
-	Item(int itemNO, int count) : ItemNO(itemNO), Count(count) {}
-
-	int GetItemNO() const { return ItemNO; }
-	int GetCount() const { return Count; }
-	void AddCount(int add) { Count += add; }
-
-	void Print()
-	{
-		printf("Item : ItemNO(%d), Count(%d)\n", ItemNO, Count);
-	}
+    int workIndex;      // 작업 구분을 위한 변수
+    int requestTime;    // 고객 서비스 요청 시간
+    int serviceTime;    // 고객이 요청한 서비스를 처리하는데 드는 시간
+    int startTime;      // 고객의 서비스가 시작되는 시간
+    int endTime;        // 고객의 서비스가 종료되는 시간
+public:
+    Job(int index, int i, int j) {
+        workIndex = index;
+        requestTime = i;
+        serviceTime = j;
+        startTime = 0;
+        endTime = 0;
+    }
 };
 
-//인벤토리 클래스
-class Inventory
-{
-	std::list<Item*> ItemList; //아이템 리스트
+// priority Queue에 들어가는 값의 비교를 위해 사용
+struct compare {
+    bool operator()(Job* a, Job* b) {
+        //     ★ 문제 1 --------------------------------------
+        //        우선순위 큐의 넣기 위해서 Job의 크기 비교 기준을 제시 
+        return a->serviceTime > b->serviceTime;
+        //      -----------------------------------------------★
+    }
+};
 
+// 일 처리를 매니징하고 컨트롤하는 클래스
+class Jobschedule {
+    // 고객의 요청 정보들을 담는 컨테이너
+    vector<Job*> InputJobs;
+    // 먼저 처리해야 하는 작업들을 고려해서 담는 컨테이너
+    priority_queue<Job*, vector<Job*>, compare> WaitJob;
+    // 최종적으로 순서대로 처리되는 작업들을 담는 컨테이너
+    vector<Job*> JobSchedule;
+    int count;              // 현재 처리중인 작업의 인덱스
+    Job* cJob;
+    int serviceStartTime;   // 현재 처리중인 작업 시작 시간
+    int serviceEndTime;     // 현재 처리가 끝난 작업 시작 시간
+    int TotalProcessTime;   // 현재까지의 각 요청에 대한 요청부터 처리까지 걸린 시간
 public:
-	//모든 아이템을 출력하는 함수
-	void Print()
-	{
-		if (ItemList.empty())
-		{
-			printf("Empty");
-			return;
-		}
+    Jobschedule(vector<vector<int>> jobs) {
+        count = 0;
+        cJob = NULL;
+        serviceStartTime = 0;
+        serviceEndTime = 0;
+        TotalProcessTime = 0;
+        for (int i = 0; i < jobs.size(); i++) {
+            InputJobs.push_back(new Job(i + 1, jobs[i][0], jobs[i][1]));
+        }
+    }
+    ~Jobschedule() {
+        for (int i = 0; i < InputJobs.size(); i++) {
+            delete InputJobs[i];
+        }
+    }
+    void JobSearch(int Time) {
+        while (count < InputJobs.size()) {
+            if (InputJobs[count]->requestTime <= Time) //cJob->serviceTime)
+            {
+                WaitJob.push(InputJobs[count]);
+                count++;
+            }
+            else
+                break;
+        }
+    }
+    void JobWork() {
+        while (!WaitJob.empty()) {
+            cJob = WaitJob.top();
+            WaitJob.pop();
 
-		for (auto it = ItemList.begin(); it != ItemList.end(); ++it)
-		{
-			(*it)->Print();
-		}
-	}
-
-	//아이템을 찾는 함수
-	Item* GetItem(int itemNO)
-	{
-		for (auto it = ItemList.begin(); it != ItemList.end(); ++it)
-		{
-			if ((*it)->GetItemNO() == itemNO)
-			{
-				return (*it);
-			}
-		}
-		return nullptr; //일치하는 아이템이 없을 경우 nullptr 반환
-	}
-
-	//아이템을 추가하는 함수
-	void AddItem(int itemNO, int count)
-	{
-		//---------------------------------------------------------------------------------------------------------------
-		//★ 문제 1
-		//매개변수로 전달된 itemNO 와 같은 아이템이 있을 경우 해당 아이템의 Count 를 매개변수로 전달된 count 만큼 증가시키고,
-		//없을 경우 새로운 아이템을 추가하시오.
-		//---------------------------------------------------------------------------------------------------------------
-
-		if (GetItem(itemNO) != nullptr) {
-			GetItem(itemNO)->AddCount(count);
-		}
-		else if (GetItem(itemNO) == nullptr) {
-			Item* instance = new Item(itemNO, count);
-			ItemList.push_back(instance);
-		}
-	}
-
-	//아이템을 제거하는 함수
-	void RemoveItems(std::set<int> removeList)
-	{
-		//---------------------------------------------------------------------------------------------------------------
-		//★ 문제 2
-		//매개변수로 전달된 removeList 와 일치하는 모든 아이템을 제거하시오. 동적 할당이 되어있으므로 delete 처리도 필요함!
-		//단, removeList 에는 제거하려는 itemID 들이 들어있음.
-		//---------------------------------------------------------------------------------------------------------------
-		for (auto iter = removeList.begin(); iter != removeList.end(); iter++) {
-			Item* tempItem = GetItem(*iter);
-			ItemList.remove(tempItem);
-			delete tempItem;
-		}
-	}
-
-	//아이템 리스트를 역순으로 변경하는 함수
-	void ReverseItems()
-	{
-		//---------------------------------------------------------------------------------------------------------------
-		//★ 문제 3
-		//아이템 리스트의 순서를 역순으로 변경하시오. 
-		//---------------------------------------------------------------------------------------------------------------
-
-		ItemList.reverse();
-	}
+            //     ★ 문제 2 --------------------------------------
+            //        우선순위 큐에서 대기하고 있는 요청들을 빼내서 
+            //        JobSchedule에 넣을 수 있도록 한다. 
+            //        1) cJob의 멤버 변수들(startTime, endTime)의 값을 채운다. 
+            //        2) serviceStartTime과 serviceEndTime도 값을 채운다.
+            //        3) 고객들의 각 요청에 대한 "요청 시간 부터 
+            //          서비스가 처리된 시간"까지의 시간들의 합을 
+            //          TotalProcessTime에 담는다.
+            //      ----------------------------------------------★
+            cJob->startTime = serviceEndTime;
+            cJob->endTime = cJob->startTime + cJob->serviceTime;
+            serviceStartTime = cJob->startTime;
+            serviceEndTime = cJob->endTime;
+            TotalProcessTime += cJob->endTime - cJob->requestTime;
+            JobSchedule.push_back(cJob);
+            JobSearch(serviceEndTime);
+        }
+    }
+    void JobInit() {
+        count = 0;
+        cJob = InputJobs[count];
+        WaitJob.push(cJob);
+        count++;
+        JobWork();
+    }
+    void PrintJobSchedule() {
+        for (int i = 0; i < JobSchedule.size(); i++) {
+            cout << "[ Work Index : " << JobSchedule[i]->workIndex << " ]" << endl;
+            cout << " 1. Work Request Time : " << JobSchedule[i]->requestTime << endl;
+            cout << " 2. Work Start Time : " << JobSchedule[i]->startTime << endl;
+            cout << " 3. Work End Time :" << JobSchedule[i]->endTime << endl;
+            cout << " 4. Total Processing Time : " << JobSchedule[i]->endTime - JobSchedule[i]->requestTime << endl;
+        }
+        cout << "------------------------------------" << endl;
+        cout << "Total Processing Average Time : " << (float)TotalProcessTime / JobSchedule.size() << endl;
+    }
 };
 
 int main()
 {
-	system("cls");
-	SetConsoleTitle(_T("문제"));
+    printf("■ [문제2] 코드 작성  \n■ 수험번호: 수험번호 기재 \n■ 성    명: 성명 기재\n");
+    printf("\n■  우선순위 큐 응용 ■ \n");
 
-	//★ [필수] 수험생의 수험번호와 이름을 여기에 기입하여 출력될 수 있도록 함
-	//   [주의] 수험번호와 이름이 출력되지 않을 경우 오답처리!
-	printf(">> 수험번호 : \n");
-	printf(">> 이    름 : 나장호 \n\n");
 
-	Inventory inven;
-	inven.Print();
+    //vector<vector<int>> jobs = { {0,5}, {1,4},{2,5},{3,3},{4,3}, {5,3},{6,4},{7,3},{8,5} };
+    vector<vector<int>> jobs = { {0,5}, {1,6},{2,4},{3,3} };
 
-	inven.AddItem(4, 300);
-	inven.AddItem(1, 50);
-	inven.AddItem(2, 100);
-	inven.AddItem(1, 50);
-	inven.AddItem(3, 300);
-	inven.AddItem(2, 100);
-	inven.AddItem(5, 500);
-	inven.AddItem(4, 100);
-	printf("\n\nAfter add...\n");
-	inven.Print();
+    Jobschedule jobWork(jobs);
+    jobWork.JobInit();
+    jobWork.PrintJobSchedule();
 
-	inven.RemoveItems({ 2, 3 });
-	printf("\nAfter remove...\n");
-	inven.Print();
-
-	inven.ReverseItems();
-	printf("\nAfter reverse...\n");
-	inven.Print();
-
-	getchar();
+    int a;
+    cin >> a;
 }
